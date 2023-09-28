604ea15c54256e2fab0904582041189bc3d38934
Eduardo Manucha <edumanucha@gmail.com>
2023-09-27 23:31:49 -0300

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,57 @@
+import { useEffect, useReducer } from "react";
+import { todoReducerApp } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducerApp, [], init);
+
+  useEffect(() => {
+    // console.log(todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add todo",
+      payload: todo,
+    };
+    dispatch(action);
+    // dispatch la funcion que envia la accion
+  };
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const todosCount = () => {
+    return todos.length;
+  }
+
+  const pendingTodosCount = () => {
+
+    return todos.filter((todo) => !todo.done).length;
+  }
+
+  return {
+    todos,
+    handleDeleteTodo,
+    handleNewTodo,
+    handleToggleTodo,
+    todosCount,
+    pendingTodosCount,
+  };
+};
